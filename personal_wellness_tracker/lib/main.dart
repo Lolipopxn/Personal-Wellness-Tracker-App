22430c23570532082066f83364cc03d5c9c8d033
Alifriduwan <6510110565@email.psu.ac.th>
2025-07-23 14:33:15 +0700

    install package firebase

--- personal_wellness_tracker/lib/main.dart
+++ personal_wellness_tracker/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'pages/register_page.dart';
 import 'pages/home.dart';
 import 'pages/login.dart';
 import 'pages/profile.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -20,6 +26,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => HomePage(),
         '/login': (context) => Login(),
         '/profile': (context) => Profile(),
+        '/register': (context) => RegisterPage(),
       },
     );
   }
